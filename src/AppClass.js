2b31f8746652231ba4949b79b37e76ef2115f772
CuneytYilmaz <cuneyilmaz@gmail.com>
2020-01-12 18:19:48 +0300

    useEffect added to hooks project

--- src/AppClass.js
+++ src/AppClass.js
@@ -6,6 +6,14 @@ class AppClass extends Component {
     isOn: false,
   }
 
+  componentDidMount () {
+    document.title = `I was clicked ${this.state.count} times`
+  }
+
+  componentDidUpdate () {
+    document.title = `I was clicked ${this.state.count} times`
+  }
+
   incrementCount = () => {
     this.setState(prevState => ({
       count: prevState.count + 1
