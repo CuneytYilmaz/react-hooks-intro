30278673afbf078d3bafed1203ff811dbc0f4d93
CuneytYilmaz <cuneyilmaz@gmail.com>
2020-01-12 21:23:00 +0300

    Login form created and managed state by useState

---
+++ src/Login.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+export default function Login () {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [userData, setUserData] = useState('')
+
+    const handleSubmit = event => {
+        event.preventDefault()
+
+        const userData = {
+            username,
+            password
+        }
+
+        setUserData(userData)
+        setUsername('')
+        setPassword('')
+    }
+
+    return (
+        <div style={{
+            textAlign: 'center'
+        }}>
+            <form
+                onSubmit={handleSubmit}
+                style= {{
+                    display: 'grid',
+                    alignItems: 'center',
+                    justifyItems: 'center'
+                }}>
+                <input
+                    onChange={ event => setUsername(event.target.value) }
+                    type='text'
+                    placeholder='username'
+                    value={username}
+                />
+                <input 
+                    onChange={ event => setPassword(event.target.value) }
+                    type='password'
+                    placeholder='password'
+                    value={password}
+                />
+                <button type='submit'>Login</button>
+            </form>
+
+            {userData ? JSON.stringify(userData) : null}
+        </div>
+    )
+}
